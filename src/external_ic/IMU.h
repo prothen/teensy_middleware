7569dfd00af3ba47db7e8df1100cd738e4048c97
Kaj Munhoz Arfvidsson <kajarfvidsson@gmail.com>
2023-11-27 12:33:48 +0100

    add rough sketch of structuring the code with classes

--- src/external_ic/IMU.h
+++ src/external_ic/IMU.h
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <string.h>
 
 // Deps needed to interface with the IMU
 #include "sensor_msgs/Imu.h"
@@ -19,98 +20,100 @@
 
 #include <utility/imumaths.h>
 
-std_msgs::Header header;
-
 #define BNO055_SAMPLERATE_DELAY_MS (100)
-Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire1);
-bool setupIMU() {
-    header.frame_id = "imu";
-    header.seq = 0;
-    if (!bno.begin()) {
-        /* There was a problem detecting the BNO055 ... check your connections */
-        Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-        header.frame_id = "imu-disconnected";
-        return false;
-    }
-    bno.setExtCrystalUse(true);
-    Serial.println("BN0055 detected");
-    return true;
-}
-// ros::Time now,
-void IMUReadingToMsg() {
 
-    // Header stuff
-    header.stamp = nh.now();
-    header.seq++;
+namespace SVEA {
 
-    MSG_IMU.header = header;
-    MSG_MAG.header = header;
-    MSG_TEMP.header = header;
+class IMU {
 
-    imu::Quaternion quat = bno.getQuat();
-    MSG_IMU.orientation.x = quat.x();
-    MSG_IMU.orientation.y = quat.y();
-    MSG_IMU.orientation.z = quat.z();
-    MSG_IMU.orientation.w = quat.w();
-    imu::Vector<3> Vec;
-    Vec = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-    MSG_IMU.angular_velocity.x = Vec.x();
-    MSG_IMU.angular_velocity.y = Vec.y();
-    MSG_IMU.angular_velocity.z = Vec.z();
+    Adafruit_BNO055 bno;
 
-    Vec = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-    MSG_IMU.linear_acceleration.x = Vec.x();
-    MSG_IMU.linear_acceleration.y = Vec.y();
-    MSG_IMU.linear_acceleration.z = Vec.z();
+    SVEA::NodeHandle &nh;
+    ros::Publisher imu_pub;
+    ros::Publisher imu_mag;
+    ros::Publisher imu_temp;
 
-    Vec = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
-    MSG_MAG.magnetic_field.x = Vec.x();
-    MSG_MAG.magnetic_field.y = Vec.y();
-    MSG_MAG.magnetic_field.z = Vec.z();
+    std_msgs::Header header;
+    sensor_msgs::Imu imu_msg;
+    sensor_msgs::MagneticField mag_msg;
+    sensor_msgs::Temperature temp_msg;
 
-    MSG_TEMP.temperature = bno.getTemp();
+  public:
 
-    // TODO, make more efficient or make sensible covarience, or both
-    int fakeCovariance = 0;
-    for (int i = 0; i < 9; ++i) {
-        MSG_IMU.orientation_covariance[i] = fakeCovariance;
-        MSG_IMU.angular_velocity_covariance[i] = fakeCovariance;
-        MSG_IMU.linear_acceleration_covariance[i] = fakeCovariance;
-        MSG_MAG.magnetic_field_covariance[i] = fakeCovariance;
+    IMU(SVEA::NodeHandle &nh):
+        bno(55, 0x28, &Wire1),
+        nh(nh),
+        imu_pub("imu/data", &imu_msg),
+        imu_mag("imu/mag", &mag_msg),
+        imu_temp("imu/temp", &temp_msg)
+    {
+        nh.advertise(imu_pub);
+        nh.advertise(imu_mag);
+        nh.advertise(imu_temp);
+
+        header.frame_id = "imu";
+        header.seq = 0;
+    };
+
+    bool open() {
+        bool succ = bno.begin(OPERATION_MODE_NDOF);
+
+        if (succ) {
+            bno.setExtCrystalUse(true);
+            Serial.println("BN0055 detected");
+        } else {
+            Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+        }
+
+        return succ;
     }
 
-    imu_pub.publish(&MSG_IMU);
-    nh.spinOnce();
+    void update() {
+        // Header stuff
+        header.stamp = nh.now();
+        header.seq++;
 
-    imu_mag.publish(&MSG_MAG);
-    nh.spinOnce();
+        imu_msg.header = header;
+        mag_msg.header = header;
+        temp_msg.header = header;
 
-    imu_temp.publish(&MSG_TEMP);
-    nh.spinOnce();
-    // Serial.printf("IMU: Orientation(%f, %f, %f, %f) Angular Velocity(%f, %f, %f) Linear Acceleration(%f, %f, %f)\n", msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w, msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z, msg.linear_acceleration.x, msg.linear_acceleration.y, msg.linear_acceleration.z);
-    return;
-}
+        imu::Quaternion quat = bno.getQuat();
+        imu_msg.orientation.x = quat.x();
+        imu_msg.orientation.y = quat.y();
+        imu_msg.orientation.z = quat.z();
+        imu_msg.orientation.w = quat.w();
 
-void IMU_DEBUG() {
-    /* Get a new sensor event */
-    sensors_event_t event;
-    bno.getEvent(&event);
-    /* Board layout:
-           +----------+
-           |         *| RST   PITCH  ROLL  HEADING
-       ADR |*        *| SCL
-       INT |*        *| SDA     ^            /->
-       PS1 |*        *| GND     |            |
-       PS0 |*        *| 3VO     Y    Z-->    \-X
-           |         *| VIN
-           +----------+
-    */
-    /* The processing sketch expects data as roll, pitch, heading */
-    Serial.print(F("Orientation: "));
-    Serial.print((float)event.orientation.x);
-    Serial.print(F(" "));
-    Serial.print((float)event.orientation.y);
-    Serial.print(F(" "));
-    Serial.print((float)event.orientation.z);
-    Serial.println(F(""));
-}
+        imu::Vector<3> vec;
+        vec = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+        imu_msg.linear_acceleration.x = vec.x();
+        imu_msg.linear_acceleration.y = vec.y();
+        imu_msg.linear_acceleration.z = vec.z();
+
+        vec = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+        imu_msg.angular_velocity.x = vec.x();
+        imu_msg.angular_velocity.y = vec.y();
+        imu_msg.angular_velocity.z = vec.z();
+        
+        vec = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
+        mag_msg.magnetic_field.x = vec.x();
+        mag_msg.magnetic_field.y = vec.y();
+        mag_msg.magnetic_field.z = vec.z();
+
+        temp_msg.temperature = bno.getTemp();
+
+        int fakeCovariance = 0;
+        for (int i = 0; i < 9; ++i) {
+            imu_msg.orientation_covariance[i] = fakeCovariance;
+            imu_msg.angular_velocity_covariance[i] = fakeCovariance;
+            imu_msg.linear_acceleration_covariance[i] = fakeCovariance;
+            mag_msg.magnetic_field_covariance[i] = fakeCovariance;
+        }
+
+        imu_pub.publish(&imu_msg);
+        imu_mag.publish(&mag_msg);
+        imu_temp.publish(&temp_msg);
+    }
+
+};
+    
+} // namespace SVEA
